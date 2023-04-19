6778bdc1dda018561fb14ee2cca5202a43e845d5
zobayerdev <zobayer.dev@gmail.com>
2023-04-19 14:05:34 +0600

    navigation drawer and bottom navigation drawer code are implemented

--- app/src/main/java/com/trodev/medicarepro/activities/MainActivity.java
+++ app/src/main/java/com/trodev/medicarepro/activities/MainActivity.java
@@ -1,16 +1,182 @@
 package com.trodev.medicarepro.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.trodev.medicarepro.BuildConfig;
 import com.trodev.medicarepro.R;
+import com.trodev.medicarepro.fragments.AboutFragment;
+import com.trodev.medicarepro.fragments.HomeFragment;
+import com.trodev.medicarepro.fragments.ImageFragment;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private long pressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        // #######################
+        // Drawer Layout implement
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // #################################################################
+        // eikhane eituku hocche amader navigation layout er kaj korar jonno.
+        navigationView.setNavigationItemSelectedListener(this::onOptionsItemSelected);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        loadHomeFragment();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.bottom_menu_home) {
+                    loadHomeFragment();
+                } else if (itemId == R.id.bottom_menu_dev) {
+                    loadDeveloperFragment();
+                } else if (itemId == R.id.bottom_menu_about) {
+                    loadAboutFragment();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Click", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
     }
+
+
+    private void loadHomeFragment() {
+        setTitle("Home");
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, homeFragment, "homeFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void loadDeveloperFragment() {
+
+        setTitle("Prescription List");
+        ImageFragment imageFragment = new ImageFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, imageFragment, "imageFragment");
+        fragmentTransaction.commit();
+
+    }
+
+    private void loadAboutFragment() {
+        setTitle("About Us");
+        AboutFragment aboutFragment = new AboutFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, aboutFragment, "aboutFragment");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                //   startActivity(new Intent(MainActivity.this, CustomerReviewActivity.class));
+                Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contact us", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_reminder:
+                Toast.makeText(this, "Reminder", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_policy:
+                Toast.makeText(this, "Privacy Policy", Toast.LENGTH_SHORT).show();
+                //   startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
+                break;
+            case R.id.nav_share:
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Medicare");
+                    String shareMessage = "\nMedicare Pro Download now\n\n";
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                    Toast.makeText(this, "Share Apps", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    //e.toString();
+                }
+                break;
+            case R.id.nav_rate:
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+                    Toast.makeText(this, "Rate us", Toast.LENGTH_SHORT).show();
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+                break;
+            case R.id.nav_apps:
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=6580660399707616800")));
+                    Toast.makeText(this, "Our Apps", Toast.LENGTH_SHORT).show();
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=6580660399707616800")));
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // In this code, android lifecycle exit on 2 times back-pressed.
+    @Override
+    public void onBackPressed() {
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+
 }
