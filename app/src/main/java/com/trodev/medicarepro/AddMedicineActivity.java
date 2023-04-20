bee0a391165be14347d10e723ca5b66cbacb36b5
zobayerdev <zobayer.dev@gmail.com>
2023-04-20 20:16:06 +0600

    category added also on apps

--- app/src/main/java/com/trodev/medicarepro/AddMedicineActivity.java
+++ app/src/main/java/com/trodev/medicarepro/AddMedicineActivity.java
@@ -2,11 +2,39 @@ package com.trodev.medicarepro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Objects;
 
 public class AddMedicineActivity extends AppCompatActivity {
+
+    private ImageView appsIV;
+    private EditText nameEt, shortDesEt, indicatorsEt, dosagesEt, interactionEt, effectEt, warningsEt, conditionEt;
+
+    private Spinner AddCategory;
+    private Button uploadBtn ;
+    private final int REQ = 1;
+    private Bitmap bitmap = null;
+    private String category;
+    private ProgressDialog progressDialog;
+    private String name, description, indicators, dosages, interaction, effect, warnings, conditions; // kono edittext er valu baranor jonno eikhane nite hobe
+    private String downloadUrl = "";
+    private StorageReference storageReference;
+    private DatabaseReference reference, dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +43,41 @@ public class AddMedicineActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Add Medicine");
+
+        nameEt = findViewById(R.id.nameEt);
+        shortDesEt = findViewById(R.id.shortEt);
+        indicatorsEt = findViewById(R.id.indicatorEt);
+        dosagesEt = findViewById(R.id.doseEt);
+        interactionEt = findViewById(R.id.interactionEt);
+        effectEt = findViewById(R.id.effectEt);
+        warningsEt = findViewById(R.id.warningsEt);
+        conditionEt = findViewById(R.id.conditionEt);
+        AddCategory = findViewById(R.id.AddCategory);
+
+        progressDialog = new ProgressDialog(this);
+
+        // firebase
+        reference = FirebaseDatabase.getInstance().getReference().child("Medicines");
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+
+        String[] items = new String[]{"Select Category", "Powder for suspension", "Capsules ", "Tablet", "Ointment", "Inhaler", "Drops", "Suppositories", "Injection "};
+        AddCategory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items));
+
+        
+        AddCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { // eikhane change ache AddStudentCategory
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                category = AddCategory.getSelectedItem().toString(); // eikhane change ache AddStudentCategory
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/trodev/medicarepro/adapters/MedicineAdapters.java
+++ app/src/main/java/com/trodev/medicarepro/adapters/MedicineAdapters.java
@@ -1,4 +1,48 @@
 package com.trodev.medicarepro.adapters;
 
-public class MedicineAdapters {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.trodev.medicarepro.models.MedicineModels;
+
+import java.util.List;
+
+public class MedicineAdapters extends RecyclerView.Adapter<MedicineAdapters.MedicineViewHolder> {
+
+    // eikhnae amra list declear korbo kon seikhaner list er Data ta check korte hobe
+    private List<MedicineModels> list;
+    private Context context;
+    private String category;
+
+    public MedicineAdapters(List<MedicineModels> list, Context context, String category) {
+        this.list = list;
+        this.context = context;
+        this.category = category;
+    }
+
+    @NonNull
+    @Override
+    public MedicineAdapters.MedicineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MedicineAdapters.MedicineViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MedicineViewHolder extends RecyclerView.ViewHolder {
+        public MedicineViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
